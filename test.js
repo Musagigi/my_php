00a60b6500187e144a10fb2a2021849459b46f53
Musajiji <dzhal@bk.ru>
2023-11-21 17:53:56 +0300

    -

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-console.log(test);
+let t = Math.random()
+console.log(t, t * 10);
 let test = 5;
+var myData = "Manchester,London,Liverpool,Birmingham,Leeds,Carlisle";
+let myArr = myData.split(',')
+console.log(myArr.toString());
